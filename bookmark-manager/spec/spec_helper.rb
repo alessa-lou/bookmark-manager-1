866c4871c309982c029dc15ba047b8f48e498522
Alessa <alessa_hardwick@live.co.uk>
2020-07-01 17:35:22 +0100

    Set up test environment

--- bookmark-manager/spec/spec_helper.rb
+++ bookmark-manager/spec/spec_helper.rb
@@ -13,6 +13,8 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require_relative './setup_test_database'
+
 
 ENV['RACK_ENV'] = 'test'
 ENV['ENVIRONMENT'] = 'test'
@@ -25,19 +27,13 @@ require 'rspec'
 Capybara.app = BookmarkManager
 
 RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
-  config.expect_with :rspec do |expectations|
-    # This option will default to `true` in RSpec 4. It makes the `description`
-    # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chain`, e.g.:
-    #     be_bigger_than(2).and_smaller_than(4).description
-    #     # => "be bigger than 2 and smaller than 4"
-    # ...rather than:
-    #     # => "be bigger than 2"
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  config.before(:each) do
+    setup_test_database
   end
+  # config.expect_with :rspec do |expectations|
+  #
+  #   expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  # end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
